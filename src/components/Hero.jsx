9890e8174da2179d18b835d7ec26f74577cc38cf
Michele <mikzed@hotmail.it>
2025-03-25 16:51:19 +0100

    Link added to buttons

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { IoArrowForwardCircle } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 function Hero() {
   const services = [
@@ -42,71 +43,82 @@ function Hero() {
         </p>
         <p className="mt-4 text-xl lg:text-2xl">Customized care starts here</p>
         <div className="grid md:grid-cols-2 items-start mt-4 text-lg font-semibold duration-200 ease-in-out transition-all gap-2">
-          {/* "Lose weight" button */}
-          <button className="group flex justify-between py-6 px-4 bg-neutral-100 rounded-2xl w-full hover:bg-gradient-to-r hover:from-amber-700 hover:to-amber-200 hover:text-white duration-200 ease-in-out transition-all cursor-pointer">
-            <div>
-              Lose <span className="text-amber-800">weight</span>
-            </div>
-            <div className="flex gap-3">
-              <img
-                src="/media/double1.png"
-                className="w-11 group-hover:hidden"
-              />
-              <img
-                src="/media/double2.png"
-                className="w-11 hidden group-hover:block"
-              />
-              <IoArrowForwardCircle size={30} className="text-black" />
-            </div>
-          </button>
-          {/* "Regrow hair" button */}
-          <button className="group flex justify-between py-6 px-4 bg-neutral-100 rounded-2xl w-full hover:bg-gradient-to-r hover:from-red-700 hover:to-red-200 hover:text-white duration-200 ease-in-out transition-all cursor-pointer">
-            <div>
-              Tackle <span className="text-red-800">anxiety</span>
-            </div>
-            <div className="flex gap-3">
-              <img
-                src="/media/white1.png"
-                className="w-12 group-hover:hidden"
-              />
-              <img
-                src="/media/white2.png"
-                className="w-12 hidden group-hover:block"
-              />
-              <IoArrowForwardCircle size={30} className="text-black" />
-            </div>
-          </button>
-          {/* "Have better sex" button */}
-          <button className="group flex justify-between py-6 px-4 bg-neutral-100 rounded-2xl w-full hover:bg-gradient-to-r hover:from-sky-700 hover:to-sky-200 hover:text-white duration-200 ease-in-out transition-all cursor-pointer">
-            <div>
-              Have better <span className="text-sky-800">sex</span>
-            </div>
-            <div className="flex gap-3">
-              <img src="/media/blue1.png" className="w-16 group-hover:hidden" />
-              <img
-                src="/media/blue2.png"
-                className="w-16 hidden group-hover:block"
-              />
-              <IoArrowForwardCircle size={30} className="text-black" />
-            </div>
-          </button>
-          {/* "Tackle anxiety" button */}
-          <button className="group flex justify-between py-6 px-4 bg-neutral-100 rounded-2xl w-full hover:bg-gradient-to-r hover:from-green-700 hover:to-green-200 hover:text-white duration-200 ease-in-out transition-all cursor-pointer">
-            <div>
-              Regrow <span className="text-green-800">hair</span>
-            </div>
-            <div className="flex gap-3">
-              <img
-                src="/media/green1.png"
-                className="w-10 group-hover:hidden"
-              />
-              <img
-                src="/media/green2.png"
-                className="w-10 hidden group-hover:block"
-              />
-              <IoArrowForwardCircle size={30} className="text-black" />
-            </div>
-          </button>
+          {/* "Weight loss" button */}
+          <Link to={"/weight-loss"}>
+            <button className="group flex justify-between py-6 px-4 bg-neutral-100 rounded-2xl w-full hover:bg-gradient-to-r hover:from-amber-700 hover:to-amber-200 hover:text-white duration-200 ease-in-out transition-all cursor-pointer">
+              <div>
+                Lose <span className="text-amber-800">weight</span>
+              </div>
+              <div className="flex gap-3">
+                <img
+                  src="/media/double1.png"
+                  className="w-11 group-hover:hidden"
+                />
+                <img
+                  src="/media/double2.png"
+                  className="w-11 hidden group-hover:block"
+                />
+                <IoArrowForwardCircle size={30} className="text-black" />
+              </div>
+            </button>
+          </Link>
+          {/* "Mental health" button */}
+          <Link to={"/mental-health"}>
+            <button className="group flex justify-between py-6 px-4 bg-neutral-100 rounded-2xl w-full hover:bg-gradient-to-r hover:from-red-700 hover:to-red-200 hover:text-white duration-200 ease-in-out transition-all cursor-pointer">
+              <div>
+                Tackle <span className="text-red-800">anxiety</span>
+              </div>
+              <div className="flex gap-3">
+                <img
+                  src="/media/white1.png"
+                  className="w-12 group-hover:hidden"
+                />
+                <img
+                  src="/media/white2.png"
+                  className="w-12 hidden group-hover:block"
+                />
+                <IoArrowForwardCircle size={30} className="text-black" />
+              </div>
+            </button>
+          </Link>
+          {/* "Sexual health" button */}
+          <Link to={"/sexual-health"}>
+            <button className="group flex justify-between py-6 px-4 bg-neutral-100 rounded-2xl w-full hover:bg-gradient-to-r hover:from-sky-700 hover:to-sky-200 hover:text-white duration-200 ease-in-out transition-all cursor-pointer">
+              <div>
+                Have better <span className="text-sky-800">sex</span>
+              </div>
+              <div className="flex gap-3">
+                <img
+                  src="/media/blue1.png"
+                  className="w-16 group-hover:hidden"
+                />
+                <img
+                  src="/media/blue2.png"
+                  className="w-16 hidden group-hover:block"
+                />
+                <IoArrowForwardCircle size={30} className="text-black" />
+              </div>
+            </button>
+          </Link>
+          {/* "Hair regrowth" button */}
+          <Link to={"/hair-regrowth"}>
+            <button className="group flex justify-between py-6 px-4 bg-neutral-100 rounded-2xl w-full hover:bg-gradient-to-r hover:from-green-700 hover:to-green-200 hover:text-white duration-200 ease-in-out transition-all cursor-pointer">
+              <div>
+                Regrow <span className="text-green-800">hair</span>
+              </div>
+              <div className="flex gap-3">
+                <img
+                  src="/media/green1.png"
+                  className="w-10 group-hover:hidden"
+                />
+                <img
+                  src="/media/green2.png"
+                  className="w-10 hidden group-hover:block"
+                />
+                <IoArrowForwardCircle size={30} className="text-black" />
+              </div>
+            </button>
+          </Link>
         </div>
       </div>
     </>
